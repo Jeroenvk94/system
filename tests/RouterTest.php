630b49e537e022343d1816d5a20588c4ba0d7201
U-Orest-PC\Orest <Orest@Orest-PC.(none)>
2014-07-14 17:13:20 +0300

    update router test

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -8,18 +8,20 @@
 class RouterTest extends \PHPUnit_Framework_TestCase {
 
     public $router;
+    public $routes = array();
     public $baseUrl;
+    
 
     protected function setUp() {
         $this->router = new \System\Router();
         
-        $first = new \System\Router\Route('/first');
-        $second = new \System\Router\Route('/second');
-        $third = new \System\Router\Regex('/article/:id', null, array('id' => '\d+'));
+        $this->routes['first'] = new \System\Router\Route('/first');
+        $this->routes['second'] = new \System\Router\Route('/second');
+        $this->routes['third'] = new \System\Router\Regex('/article/:id', null, array('id' => '\d+'));
         
-        $this->router->add('first', $first);
-        $this->router->add('second', $second);
-        $this->router->add('regex', $third);
+        $this->router->add('first', $this->routes['first']);
+        $this->router->add('second', $this->routes['second']);
+        $this->router->add('regex', $this->routes['third']);
     }
 
     public function testFirst() {
