ed381d46d4678d01d6e37af306aa5ba814d4deed
Orest Budzhak <doom4eg@gmail.com>
2014-12-08 10:38:27 +0200

    add redis session handler

---
+++ src/Session/Handler/RedisSessionHandler.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace System\Session\Handler\RedisSessionHandler;
+
+use System\Session,
+    Predis\Client as PredisClient;
+
+/**
+ * Description of RedisSessionHandler
+ *
+ * @author root
+ */
+class RedisSessionHandler implements \SessionHandlerInterface
+{
+
+    /**
+     *
+     * @var PredisClient 
+     */
+    protected $db;
+    protected $ttl;
+    protected $prefix = '';
+    static $salt = '';
+
+    public function __construct(PredisClient $db, $prefix = '')
+    {
+        $this->db = $db;
+        $this->prefix = $prefix;
+        $this->ttl = Session::getRememberMeTime();
+        Session::disableGarbageCollection();
+    }
+
+    public function create_sid()
+    {
+        return md5(microtime() . self::$salt);
+    }
+
+    public function open($savePath, $sessionName)
+    {
+        // No action necessary because connection is injected
+        // in constructor and arguments are not applicable.
+        return true;
+    }
+
+    public function read($id)
+    {
+        $id = $this->prefix . $id;
+        $sessData = $this->db->get($id);
+        $this->db->expire($id, $this->ttl);
+        return $sessData;
+    }
+
+    public function write($id, $data)
+    {
+        $id = $this->prefix . $id;
+        $this->db->set($id, $data);
+        $this->db->expire($id, $this->ttl);
+    }
+
+    public function destroy($id)
+    {
+        $this->db->del($this->prefix . $id);
+    }
+
+    public function gc($maxLifetime)
+    {
+        // no action necessary because using EXPIRE
+        return true;
+    }
+
+    public function close()
+    {
+        $this->db = null;
+        unset($this->db);
+    }
+
+}
